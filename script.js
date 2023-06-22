cf8302265f5de8075b93aa86cb9ef10ecd783bfa
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-22 05:33:12 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let p1;
+let p2;
+
+const btnStart = document.getElementById("btnStart");
+btnStart.addEventListener("click",function(e){
+	e.preventDefault();
+	const player1 = document.getElementById("p1");
+	const player2 = document.getElementById("p2");
+	p1 = player1.value;
+	p2 = player2.value;
+});
